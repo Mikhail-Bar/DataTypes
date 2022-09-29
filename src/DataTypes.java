516da8263c13f3beb2c384831001d0675832091f
гк <@adamant.local>
2022-09-29 18:48:39 +0300

    Initial commit

---
+++ src/DataTypes.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        boolean b =Boolean.TRUE;
+        System.out.printf("boolean: min=%b, max=%b, range=%d%n", Boolean.FALSE, Boolean.TRUE, 1);
+        System.out.printf("byte: min=%d, max=%d, range=%d%n", Byte.MIN_VALUE, Byte.MAX_VALUE, Byte.BYTES);
+        System.out.printf("short: min=%d max=%d, range=%d%n", Short.MIN_VALUE, Short.MAX_VALUE, Short.BYTES);
+        System.out.printf("int: min=%d max=%d, range=%d%n", Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.BYTES);
+        System.out.printf("long: min=%d max=%d, range=%d%n", Long.MIN_VALUE, Long.MAX_VALUE, Long.BYTES);
+        System.out.printf("float: min=%f max=%e, range=%d%n", Float.MIN_VALUE, Float.MAX_VALUE, Float.BYTES);
+        System.out.printf("double: min=%f max=%f, range=%d%n", Double.MIN_VALUE, Double.MAX_VALUE, Double.BYTES);
+
+
+        boolean weather = true;
+        int age = 30;
+        long population = 7_500_000_000L;
+        byte number = 5;
+        byte apple, peaches = 3, cherries =2;
+        float interest = 0,05f;
+        int day = 7;
+        char firstletter = 'A';
+        int count = 10;
+        double mass = 1.2;
+        String name = "Mikhail";
+        boolean = true;
+
+    }
+}
---
+++ src/com/company/Assignments.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Assignments Main <i, n, x> {
+    public static void main(String[] args){
+
+
+
+    double x = 1.5;
+    int n = 0;
+    n++
+
+    x = 1;
+    double y = - 2.7 * x * x * x + 0.23 * x * x - 1.4;
+
+    double pounds = 1;
+    double kilos = pounds * 0.4059;
+
+
+    double u = 220;
+    double r = 15;
+    double i = u / r;
+        System.out.println(i);
+
+}
+
+}
